9358770cb4326c3bf1176564bada551839384a74
Sebastiaan van Kuyk <sebastiaan.vankuyk@gmail.com>
2017-09-06 21:38:03 +0200

    contract finished

--- split-it.sol
+++ split-it.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.0;
 
-Contract SplitIt {
+contract SplitIt {
 
     address[] employees = [0xcBFc2Df5c8b89Bb2D8c3f38dB8d11ddD2e63dC89,
                             0x9b295eE2aBdDec61cF7BB480266079572ceB68dB,
@@ -8,19 +8,41 @@ Contract SplitIt {
                             0x88cc3F2D07B0131F8109E035B62A3BddC7F07526,
                             0x9abD2d87aD1f4ae6D9E49C41DD1fB8B65306Fa4f];
 
+    uint totalReceived = 0;
+    mapping (address => uint) withdrawnAmounts;
+
     function SplitIt() payable {
-
-
+        updateTotalReceived();
     }
 
     function () payable {
-
-
+        updateTotalReceived();
     }
 
-    function withdraw() {
+    function updateTotalReceived() internal {
+        totalReceived += msg.value;
+    }
 
+    modifier canWithdraw() {
+        bool contains = false
+        for(uint i = 0; i < employees.length; i++) {
+          if(employees[i] == msg.sender) {
+            contains = true;
+          }
+        }
+        require(contains);
+        _;
+    }
 
+    function withdraw() canWithdraw {
+        uint amountAllocated = totalReceived/employees.length;
+        uint amountWithdrawn = withdrawnAmounts[msg.sender];
+        uint amount = amountAllocated - amountWithdrawn;
+        withdrawnAmounts[msg.sender] = amountWithdrawn + amount;
+
+        if (amount > 0) {
+          msg.sender.transfer(amount);
+        }
     }
 
 }
