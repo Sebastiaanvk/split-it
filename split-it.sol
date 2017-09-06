4909eb5cfb21c728bfca96e50aea0c55a905952e
Sebastiaan van Kuyk <sebastiaan.vankuyk@gmail.com>
2017-09-06 20:53:52 +0200

    payable functions structure

---
+++ split-it.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.0;
+
+Contract SplitIt {
+
+    address[] employees = [0xcBFc2Df5c8b89Bb2D8c3f38dB8d11ddD2e63dC89,
+                            0x9b295eE2aBdDec61cF7BB480266079572ceB68dB,
+                            0xa5472233A9FF8409107EF5d71B58f92539b11274,
+                            0x88cc3F2D07B0131F8109E035B62A3BddC7F07526,
+                            0x9abD2d87aD1f4ae6D9E49C41DD1fB8B65306Fa4f];
+
+    function SplitIt() payable {
+
+
+    }
+
+    function () payable {
+
+
+    }
+
+    function withdraw() {
+
+
+    }
+
+}
